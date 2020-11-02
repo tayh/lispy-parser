9c64f477c4e563b1fec9c969f3723798bca502b0
Taynara Carvalho <tayhcarvalho@gmail.com>
2020-11-02 16:49:01 -0300

    Create grammar

--- parser.py
+++ parser.py
@@ -1,5 +1,5 @@
 import re
-from lark import Lark, InlineTransformer
+from lark import Lark, InlineTransformer, v_args
 from typing import NamedTuple
 
 
@@ -9,8 +9,35 @@ class Symbol(NamedTuple):
 
 grammar = Lark(
     r"""
-start : "implemente-aqui!"
-""")
+        ?start : expr+
+
+            ?expr : atom
+                | list
+                | quote 
+            
+
+            list  : "(" expr+ ")"
+
+            quote : "'" expr
+            
+            ?atom : STRING -> string
+                | NUMBER -> num
+                | SYMBOL -> symbol
+                | BOOLEAN -> bool
+                | NAME -> name
+                | CHAR -> char
+
+            STRING : /\"[^"\\]*(\\[^\n\t\r\f][^"\\]*)*\"/
+            SYMBOL : /[-+=\/*!@$^&~<>?<=]+/
+            NUMBER : /\+?\-?\d+(\.\d*)?([eE][+-]?\d+)?/
+            BOOLEAN: /\#t|\#f/
+            NAME   : /[a-zA-Z][-?\w]*/
+            CHAR   : /\#\\[\w]+|\d+/
+
+            %ignore /\s+/
+            %ignore /\;([^\n\r]+)/
+    """
+)
 
 
 class LispyTransformer(InlineTransformer):
@@ -26,3 +53,29 @@ class LispyTransformer(InlineTransformer):
         "space": " ",
         "tab": "\t",
     }
+
+    def atom(self, tk):
+        return tk
+    
+    def comment(self, tk):
+        return tk
+
+    def number(self, tk):
+        return float(tk)
+
+    def true(self):
+        return True
+    
+    def false(self):
+        return False
+    
+    def null(self):
+        return None
+
+    def array(self, *args):
+        return list(args)
+
+    def object(self, *args):
+        raise NotImplementedError
+
+
